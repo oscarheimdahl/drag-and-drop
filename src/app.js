277d0897d0592cb2b8d9abfe0847b2ea8d726ded
oscarheimdahl <o.heimdahl@gmail.com>
2023-06-15 12:51:57 +0200

    init

---
+++ src/app.js
@@ -0,0 +1,120 @@
+import konva from 'konva';
+
+const stage = new konva.Stage({
+  container: 'container',
+  width: window.innerWidth,
+  height: window.innerHeight,
+});
+
+const layer = new konva.Layer();
+
+for (let i = 0; i < 100; i++) {
+  const dim = 50;
+  const rect = new konva.Rect({
+    width: dim,
+    height: dim,
+    x: Math.random() * (window.innerWidth - dim),
+    y: Math.random() * (window.innerHeight - dim),
+    fill: `hsl(${Math.floor(Math.random() * 255)} 80% 70%)`,
+    draggable: true,
+    id: i,
+  });
+
+  layer.add(rect);
+}
+
+layer.children.forEach((thisRect) => {
+  thisRect.addEventListener('mouseup', () => {
+    snap(thisRect);
+  });
+  thisRect.addEventListener('dragmove', () => {
+    highlightSnap(thisRect);
+  });
+  thisRect.addEventListener('dragend', () => {
+    thisRect.opacity(1);
+  });
+});
+
+function highlightSnap(thisRect) {
+  thisRect.moveToTop();
+  let snapFound = false;
+  layer.children.forEach((otherRect) => {
+    const sameRect = otherRect.id() === thisRect.id();
+    if (sameRect) return;
+    const canSnap = canSnapTo(thisRect, otherRect);
+    if (canSnap) {
+      thisRect.opacity(0.5);
+      snapFound = true;
+    } else if (!snapFound) thisRect.opacity(1);
+  });
+  snapFound = false;
+}
+
+function snap(thisRect) {
+  layer.children.forEach((otherRect) => {
+    const sameRect = otherRect.id() === thisRect.id();
+    if (sameRect) return;
+
+    const direction = canSnapTo(thisRect, otherRect);
+    if (!direction) return;
+    switch (direction) {
+      case 'top': {
+        const otherYTop = otherRect.y() - otherRect.height();
+        thisRect.y(otherYTop);
+        thisRect.x(otherRect.x());
+        break;
+      }
+      case 'left': {
+        const otherXLeft = otherRect.x() - otherRect.width();
+        thisRect.x(otherXLeft);
+        thisRect.y(otherRect.y());
+        break;
+      }
+      case 'right': {
+        const otherXRight = otherRect.x() + otherRect.width();
+        thisRect.x(otherXRight);
+        thisRect.y(otherRect.y());
+        break;
+      }
+      case 'bottom': {
+        const otherYBottom = otherRect.y() + otherRect.height();
+        thisRect.y(otherYBottom);
+        thisRect.x(otherRect.x());
+        break;
+      }
+    }
+  });
+}
+
+//doesnt work well with none square rects
+function canSnapTo(thisRect, otherRect) {
+  const snapDistance = thisRect.width() / 4;
+  const otherXRight = otherRect.x() + otherRect.width();
+  const otherXLeft = otherRect.x() - otherRect.width();
+  const otherYBottom = otherRect.y() + otherRect.height();
+  const otherYTop = otherRect.y() - otherRect.height();
+  const distanceToRedRight = Math.abs(thisRect.x() - otherXRight);
+  const distanceToRedLeft = Math.abs(thisRect.x() - otherXLeft);
+  const distanceToRedBottom = Math.abs(thisRect.y() - otherYBottom);
+  const distanceToRedTop = Math.abs(thisRect.y() - otherYTop);
+  const xWithinSnap = Math.abs(otherRect.x() - thisRect.x()) < snapDistance;
+  const yWithinSnap = Math.abs(otherRect.y() - thisRect.y()) < snapDistance;
+
+  if (yWithinSnap) {
+    if (distanceToRedLeft < snapDistance) {
+      return 'left';
+    }
+    if (distanceToRedRight < snapDistance) {
+      return 'right';
+    }
+  }
+  if (xWithinSnap) {
+    if (distanceToRedTop < snapDistance) {
+      return 'top';
+    }
+    if (distanceToRedBottom < snapDistance) {
+      return 'bottom';
+    }
+  }
+}
+stage.add(layer);
